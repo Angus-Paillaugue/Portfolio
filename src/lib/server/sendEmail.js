b12f889376cd717d92a9173fb8ce68ba9e46c6c3
mere-patrie <angus.paillaugue40@gmail.com>
2023-06-28 23:44:38 +0200

    a

--- src/lib/server/sendEmail.js
+++ src/lib/server/sendEmail.js
@@ -1,5 +1,5 @@
 // import { Message, SMTPClient, } from 'emailjs';
-import { EMAIL_APP_PASSWORD } from "$env/static/private"
+// import { EMAIL_APP_PASSWORD } from "$env/static/private"
 
 // Use the emailjs library to set up an SMTP client using your credentials
 // const client = new SMTPClient({
@@ -9,22 +9,20 @@ import { EMAIL_APP_PASSWORD } from "$env/static/private"
 //     ssl: true,
 // });
 
-// General function to send an email to a single address 
-// from your chosen `from` email
-export async function sendEmail({ subject, text, attachment }) {
-    // const msg = new Message({
-    //     text,
-    //     from: 'angus.paillaugue40@gmail.com',
-    //     to: "angus.paillaugue40@gmail.com",
-    //     subject,
-    //     attachment: attachment ?? [],
-    // });
+// export async function sendEmail({ subject, text, attachment }) {
+//     const msg = new Message({
+//         text,
+//         from: 'angus.paillaugue40@gmail.com',
+//         to: "angus.paillaugue40@gmail.com",
+//         subject,
+//         attachment: attachment ?? [],
+//     });
 
-    try {
-        // await client.sendAsync(msg);
-    } catch (error) {
-        console.log(error)
-    }
+//     try {
+//         await client.sendAsync(msg);
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-    return;
-}
+//     return;
+// }
