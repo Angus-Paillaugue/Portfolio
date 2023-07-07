d04eb1ab3668aefe3643a1e527bf898bf18b6310
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-07 21:53:23 +0200

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     let message;
     let sendEmailButtonContent = "Submit";
 
+
     async function contact(){
         sendEmailButtonContent = "<div role='status'><svg aria-hidden='true' class='inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-primary-600' viewBox='0 0 100 101' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z' fill='currentColor'/><path d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z' fill='currentFill'/></svg><span class='sr-only'>Loading...</span></div>"
         const response = await fetch('/contact', {
@@ -36,7 +37,7 @@
     <title>Angus's portfolio</title>
 </svelte:head>
 
-<section class="p-4 lg:p-10 sm:p-6 min-h-screen flex flex-col md:justify-center items-center relative">
+<section class="p-4 lg:p-10 sm:p-6 min-h-screen flex flex-col md:justify-center items-center relative" id="Home">
 
     <div class="absolute top-0 left-0 w-full h-full duration-[10s]" style="z-index: -20; background: url(https://astro.build/assets/noise.webp) repeat,linear-gradient(0deg,#FFBCAD 0%,#EF562F 100%); background-blend-mode: overlay;-webkit-mask-image: radial-gradient(rgba(0,0,0,.5),transparent 70%);"></div>
 
@@ -100,11 +101,11 @@
     <div class="absolute bottom-0 left-0 w-full h-64" style="z-index: -19;background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);"></div>
 
     
-    <section id="about" class="p-4 lg:p-10 sm:p-6 flex flex-col lg:justify-center items-center">
+    <section id="About" class="p-4 lg:p-10 sm:p-6 flex flex-col lg:justify-center items-center">
 
         <div class="w-full max-w-4xl mx-auto py-24 flex flex-col gap-10">
             
-            <div class="grid lg:grid-cols-2 grid-cols-1 items-center md:gap-10 gap-0 w-full max-w-5xl mx-auto rounded-lg lg:p-10 p-6 bg-white dark:bg-gray-700 text-start transition-all border dark:border-gray-600 border-gray-300" use:reveal={{ transition: "fly", duration:200, y:60 }}>
+            <div class="grid lg:grid-cols-2 grid-cols-1 items-center md:gap-10 gap-0 w-full mx-auto rounded-lg lg:p-10 p-6 bg-white dark:bg-gray-700 text-start transition-all border dark:border-gray-600 border-gray-300" use:reveal={{ transition: "fly", duration:200, y:60 }}>
                 <div class="w-full lg:w-auto h-full lg:aspect-auto aspect-square relative lg:-mt-0 -mt-20 lg:-ml-20 -ml-0 rounded-lg bg-cover bg-no-repeat bg-center" style="background-image: url(aboutImg.jpg);">
                     <div class="absolute top-0 left-0 h-full w-full z-10 opacity-70 rounded-2xl" style="background: url(noise.png) repeat;background-blend-mode: overlay;"></div>
                 </div>
@@ -126,7 +127,7 @@
         
     </section>
 
-    <section id="work" class="p-4 lg:p-20 sm:p-6">
+    <section id="Work" class="p-4 lg:p-20 sm:p-6">
         <div class="w-full max-w-4xl mx-auto py-24 flex flex-col gap-10">
             <h4 class="text-primary-600 font-extrabold">MY PROJECTS</h4>
     
@@ -153,7 +154,7 @@
         </div>
     </section>
     
-    <section id="experience" class="p-4 lg:p-10 sm:p-6">
+    <section id="Experience" class="p-4 lg:p-10 sm:p-6">
     
         <div class="w-full max-w-4xl mx-auto py-24 flex flex-col gap-10">
             <h4 class="text-primary-600 font-extrabold">MY EXPERIENCES</h4>
@@ -170,7 +171,7 @@
         </div>
     </section>
 
-    <section id="contact" class="p-4 lg:p-10 sm:p-6">
+    <section id="Contact" class="p-4 lg:p-10 sm:p-6">
     
         <div class="w-full max-w-4xl mx-auto py-24 flex flex-col gap-10">
             <form on:submit|preventDefault={contact} class="rounded-lg md:p-10 p-6 items-start bg-white dark:bg-gray-700 text-start transition-all border dark:border-gray-600 border-gray-300 flex flex-col gap-5 w-full" use:reveal={{ transition: "fly", duration:200, y:60 }}>
