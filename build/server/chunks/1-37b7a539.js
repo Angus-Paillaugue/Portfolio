0e7827efe328f31e9031506f5a312461b2b0373b
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-07 19:42:56 +0200

    a

--- build/server/chunks/1-37b7a539.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_error.svelte-ad43219b.js')).default;
-const imports = ["_app/immutable/nodes/1.42712fe5.js","_app/immutable/chunks/scheduler.0b304ea0.js","_app/immutable/chunks/index.c9e9b062.js","_app/immutable/chunks/singletons.538fc4c0.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-37b7a539.js.map
---
+++ src/lib/projects/LinkTrim.md
@@ -0,0 +1,1 @@
+This website built with Sveltekit is used to shorten url's like bit.ly.
