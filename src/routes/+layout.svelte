8eb1a356e406e844a31c4bd1f59c6e12ebda399e
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-22 11:34:51 +0200

    Added sliding tabs to the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,32 +2,8 @@
     import "../app.css";
     import { fly } from 'svelte/transition'
     import { Analytics } from '$lib/components'
-    import { onMount } from "svelte";
 
     export let data;
-
-    onMount(() => {
-        var section = document.querySelectorAll("section");
-        var sections = {};
-        var i = 0;
-
-        section.forEach(function(e) {
-            sections[e.id] = e.getBoundingClientRect().top + window.scrollY;
-        });
-        
-        window.onscroll = function() {
-            var scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) +window.innerHeight/2;
-            
-            for (i in sections) {
-                if (sections[i] <= scrollPosition) {
-                    document.querySelectorAll('[data-section]').forEach(el => {
-                        el.classList.remove('link','border-b-2');
-                    });
-                    document.querySelector('[data-section*=' + i + ']').classList.add('link','border-b-2');
-                }
-            }
-        };
-    })
 </script>
 
 <svelte:head>
@@ -37,19 +13,6 @@
 
 <Analytics />
 
-<nav class="bg-white fixed w-full z-50 top-0 left-0 border-b border-gray-200">
-    <div class="flex flex-row justify-between mx-auto max-w-4xl p-4 ">
-        <h3 class="max-md:text-xl">Angus</h3>
-        <ul class="flex justify-center md:justify-between flex-row md:gap-8 sm:gap-4 gap-2 font-medium border-gray-100 rounded-lg bg-white md:text-base text-sm">
-            <li><a href="#Home" class="link border-primary-600 border-b-2" data-section="Home">Home</a></li>
-            <li><a href="#About" class="border-primary-600" data-section="About">About</a></li>
-            <li><a href="#Work" class="border-primary-600" data-section="Work">Work</a></li>
-            <li><a href="#Experience" class="border-primary-600" data-section="Experience">Experience</a></li>
-            <li><a href="#Contact" class="border-primary-600" data-section="Contact">Contact</a></li>
-        </ul>
-    </div>
-</nav>
-  
 {#key data.url}
     <div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
         <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,14 @@
     import { Link, Button } from "$lib/components"
     import { Tooltip, Input, Label, Textarea } from 'flowbite-svelte'
     import { reveal } from 'svelte-reveal';
+    import { onMount } from "svelte";
 
     let email;
     let message;
     let sendEmailButtonContent = "Submit";
+    let navLinkUnderline;
+    let sectionsList = []
+    let sections = {}
 
     async function contact(){
         sendEmailButtonContent = "<div role='status'><svg aria-hidden='true' class='inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-primary-600' viewBox='0 0 100 101' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z' fill='currentColor'/><path d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z' fill='currentFill'/></svg><span class='sr-only'>Loading...</span></div>"
@@ -30,12 +34,56 @@
     const techStack = ["HTML", "Javascript", "TailwindCSS", "SvelteKit", "MongoDB"];
     const socials = [{ name:"LinkedIn", link:"https://www.linkedin.com/in/angus-paillaugue/" }, { name:"Github", link:"https://github.com/Angus-Paillaugue" }, { name:"CodeWars", link:"https://www.codewars.com/users/Angus%20Paillaugue" }];
     // TODO : Round codeWars svg logo
+
+    onMount(() => {
+        sectionsList = document.querySelectorAll("section");
+
+        sectionsList.forEach(function(e) {
+            sections[e.id] = e.getBoundingClientRect().top + window.scrollY;
+        });
+
+        setActiveTab();
+        
+        window.onscroll = setActiveTab;
+        window.onresize = setActiveTab;
+    });
+
+    function setActiveTab() {
+        let scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight / 2;
+            
+        for (let i in sections) {
+            if (sections[i] <= scrollPosition) {
+                document.querySelectorAll('[data-section]').forEach(el => {
+                    el.classList.remove('text-primary-600');
+                });
+                let activeSection =  document.querySelector('[data-section*=' + i + ']')
+                navLinkUnderline.style.left = activeSection.offsetLeft+"px";
+                navLinkUnderline.style.width = activeSection.clientWidth+"px";
+                activeSection.classList.add("text-primary-600")
+            }
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Angus's portfolio</title>
-    <meta name="description" content="This is the portfolio of me (Angus)">
+    <meta name="description" content="This is the portfolio of Angus Paillaugue">
 </svelte:head>
+
+<nav class="bg-white fixed w-full z-50 top-0 left-0 border-b border-gray-200">
+    <div class="flex flex-row justify-center md:justify-between mx-auto max-w-4xl p-4 items-center">
+        <h3 class="max-md:text-xl max-sm:hidden">Angus</h3>
+        <div class="flex justify-center md:justify-between flex-row md:gap-8 gap-4 font-medium border-gray-100 rounded-lg bg-white md:text-base relative h-fit">
+            <a href="#Home" data-section="Home">Home</a>
+            <a href="#About" data-section="About">About</a>
+            <a href="#Work" data-section="Work">Work</a>
+            <a href="#Experience"  data-section="Experience">Experience</a>
+            <a href="#Contact" data-section="Contact">Contact</a>
+            <span bind:this={navLinkUnderline} class="h-1 transition-all top-full bg-primary-600 rounded-full absolute"></span>
+        </div>
+    </div>
+</nav>
+  
 
 <section class="p-4 lg:p-10 sm:p-6 min-h-screen flex flex-col md:justify-center items-center relative" id="Home">
 
