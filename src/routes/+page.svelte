a06bb6763e2acbec8ae74624cf71aadaf0dd01b6
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-23 21:38:09 +0200

    Random UI change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -191,7 +191,8 @@
                                 {project.link}
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 inline-block ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>
                             </Link>
-                            <p class="text-ellipsis line-clamp-4">{@html project.description} <Link href="/project/{project.title}">More info</Link></p>
+                            <p class="text-ellipsis line-clamp-4">{@html project.description} </p>
+                            <Link href="/project/{project.title}">More info</Link>
                         </div>
                         <div class="grid gap-2" style="grid-template-rows: min-content; grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));">
                             {#each project.languages as language}
