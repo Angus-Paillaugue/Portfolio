ac74323d99cb77938c31e36cf843562567fa1599
mere-patrie <angus.paillaugue40@gmail.com>
2023-09-20 10:33:24 +0200

    Changed a link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,11 +96,11 @@
             <div class="block md:w-3/5 w-full text-start h-full">
                 <h1 class="font-bold text-7xl text-center md:text-start">Full stack web dev</h1>
                 <p class="mt-4 font-normal text-lg text-center md:text-start">Hi 👋, I'm Angus. A passionate full-stack web developer based in Toulouse, France</p>
-                <div class="flex flex-row gap-6 mt-10">
+                <div class="flex flex-row gap-6 mt-10 max-md:justify-center items-center w-full">
                     <a href="{socials.filter(el => el.name === "Github")[0].link}" target="_blank" class="group">
-                        <svg xmlns="http://www.w3.org/2000/svg" target="_blank" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
                     </a>
-                    <a href="{socials.filter(el => el.name === "LinkedIn")[0].link}" class="group">
+                    <a href="{socials.filter(el => el.name === "LinkedIn")[0].link}" target="_blank" class="group">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M8 11l0 5"></path><path d="M8 8l0 .01"></path><path d="M12 16l0 -5"></path><path d="M16 16v-3a2 2 0 0 0 -4 0"></path></svg>
                     </a>
                 </div>
