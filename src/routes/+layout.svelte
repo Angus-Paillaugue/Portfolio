3d6b66497b2d59bc40d8fead1456710a1ef8bae3
mere-patrie <angus.paillaugue40@gmail.com>
2023-10-17 11:26:54 +0200

    Fixed scroll section detection. Added social link hover scale. And added a page loader.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
     import "../app.css";
     import { Analytics } from '$lib/components';
+    import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+
+    let isLoaded = false;
+    let dots = ".";
+
+    setInterval(() => {
+        dots = dots.length <= 3 ? dots + "." : ".";
+    }, 200)
+
+    onMount(() => {
+        isLoaded = true;
+    });
 </script>
 
 <svelte:head>
@@ -10,4 +23,16 @@
 
 <Analytics />
 
-<slot />
+
+{#if isLoaded}
+    <slot />
+{:else}
+    <div class="fixed top-0 left-0 w-full h-full bg-white flex flex-col justify-center items-center" in:fade={{duration:100}} out:fade={{duration:100}}>
+        <div class="flex flex-row gap-2 justify-center items-center">
+            <svg fill='none' class="w-10 h-10 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
+                <path clip-rule='evenodd' d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z' fill='currentColor' fill-rule='evenodd' />
+            </svg>
+            <p>Loading <span class="w-2 inline-block">{dots}</span></p>
+        </div>
+    </div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
     });
 
     function setActiveTab() {
-        let scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight / 2;
+        let scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight / 3;
             
         for (let i in sections) {
             if (sections[i] <= scrollPosition) {
@@ -123,10 +123,10 @@
                     <p class="mt-4 font-normal text-lg text-center md:text-start" in:fly={{y: 100, delay:100}}>Hi 👋, I'm Angus. A passionate full-stack web developer based in Toulouse, France</p>
                     <div class="flex flex-row gap-6 mt-10 max-md:justify-center items-center w-full">
                         <a href="{socials.filter(el => el.name === "Github")[0].link}" target="_blank" class="group" in:fly={{y: 100, delay:120}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200 group-hover:scale-105"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
                         </a>
                         <a href="{socials.filter(el => el.name === "LinkedIn")[0].link}" target="_blank" class="group" in:fly={{y: 100, delay:170}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M8 11l0 5"></path><path d="M8 8l0 .01"></path><path d="M12 16l0 -5"></path><path d="M16 16v-3a2 2 0 0 0 -4 0"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 group-hover:text-primary-600 transition-all duration-200 group-hover:scale-105"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M8 11l0 5"></path><path d="M8 8l0 .01"></path><path d="M12 16l0 -5"></path><path d="M16 16v-3a2 2 0 0 0 -4 0"></path></svg>
                         </a>
                     </div>
                 </div>
